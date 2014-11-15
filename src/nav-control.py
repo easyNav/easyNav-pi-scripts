0da3b7373c2c4851abdc123106e110c996a8bacb
Joel Tong <me@joeltong.org>
2014-11-15 09:19:01 +0800

    added script to control nav with pause, unpause, and reset switches

---
+++ src/nav-control.py
@@ -0,0 +1,31 @@
+## Script to control Navigation
+
+import sys
+from easyNav_pi_dispatcher import DispatcherClient
+import time
+
+arg = sys.argv[1:]
+
+if (len(arg) != 1):
+    print 'Error.  Invalid arguments.'
+    sys.exit(1)
+
+command = arg[0]
+
+DISPATCHER_PORT = 9100
+_dispatcherClient = DispatcherClient(port=DISPATCHER_PORT)
+_dispatcherClient.start()
+
+### Reset
+if (command == 'reset'):
+    _dispatcherClient.send(9001, 'reset', {})
+
+### Pause
+elif (command == 'pause'):
+    _dispatcherClient.send(9001, 'pause', {})
+
+### Unpause
+elif (command == 'unpause'):
+    _dispatcherClient.send(9001, 'unpause', {})
+
+_dispatcherClient.stop()
